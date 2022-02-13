517bd069f9cf1944728eda2dbdb69d728f8e8420
kim <kim_man@live.com>
2022-02-13 18:37:55 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HeroSection from './components/HeroSection';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Image1 from '../../images/Image1.jpg';
+import {
+  ArrowForward,
+  ArrowRight,
+  HeroBg,
+  HeroContainer,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  ImgBg,
+} from './HeroElements';
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <ImgBg src={Image1} />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Lifecare Essendon</HeroH1>
+        <HeroP>Leaders in physiotherapy and sports medicine</HeroP>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
